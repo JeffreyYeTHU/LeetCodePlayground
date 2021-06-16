2a2373a39340c3c004b2405f18a77b7e28eb29aa
Jeffrey <780336375@qq.com>
2021-06-16 18:32:12 +0800

    Add iterative traverse

--- binary_tree/src/main/java/com/jeffrey/binary_tree/BinaryTree.java
+++ binary_tree/src/main/java/com/jeffrey/binary_tree/BinaryTree.java
@@ -2,11 +2,77 @@ package com.jeffrey.binary_tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public final class BinaryTree {
-    public static List<Integer> inorderTraversal(TreeNode root) {
+    public static List<Integer> inorderRecur(TreeNode root) {
         List<Integer> traversalResult = new ArrayList<>();
-
+        inorderRecurHelper(root, traversalResult);
         return traversalResult;
+    }
+
+    public static List<Integer> preorderIter(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode currNode = root;
+        while (!nodes.empty() || currNode != null) {
+            if (currNode != null) {
+                res.add(currNode.getVal());
+                nodes.add(currNode);
+                currNode = currNode.getLeft();
+            } else {
+                var node = nodes.pop();
+                currNode = node.getRight();
+            }
+        }
+        return res;
+    }
+
+    public static List<Integer> inorderIter(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode currNode = root;
+        while (!nodes.empty() || currNode != null) {
+            if (currNode != null) {
+                nodes.add(currNode);
+                currNode = currNode.getLeft();
+            } else {
+                var node = nodes.pop();
+                res.add(node.getVal());
+                currNode = node.getRight();
+            }
+        }
+        return res;
+    }
+
+    public static List<Integer> postorderIter(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> nodes = new Stack<>();
+        nodes.add(root);
+        TreeNode curr = root;
+        TreeNode pre = null;
+        while (!nodes.empty()) {
+            curr = nodes.peek();
+            if ((curr.getLeft() == null && curr.getRight() == null)
+                    || pre != null && (pre.getLeft() == curr || pre.getRight() == curr)) {
+                res.add(curr.getVal());
+                nodes.pop();
+                pre = curr;
+            } else {
+                if (curr.getLeft() != null)
+                    nodes.push(curr.getLeft());
+                if (curr.getRight() != null)
+                    nodes.push(curr.getRight());
+            }
+        }
+        return res;
+    }
+
+    private static void inorderRecurHelper(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            inorderRecurHelper(root.getLeft(), result);
+            result.add(root.getVal());
+            inorderRecurHelper(root.getRight(), result);
+        }
     }
 }
--- binary_tree/src/test/java/com/jeffrey/binary_tree/BinaryTreeTests.java
+++ binary_tree/src/test/java/com/jeffrey/binary_tree/BinaryTreeTests.java
@@ -12,20 +12,67 @@ class BinaryTreeTests {
 	@Test
 	void inorderTraversal_normalCase() {
 		TreeNode root = generateTree();
-		var res = BinaryTree.inorderTraversal(root);
-		var actual = new ArrayList<Integer>();
-		actual.add(2);
-		actual.add(4);
-		actual.add(3);
-		actual.add(1);
+		List<Integer> res = BinaryTree.inorderRecur(root);
+		var actual = getInorderResult();
 		assertIterableEquals(res, actual);
 	}
 
-	TreeNode generateTree() {
+	@Test
+	void preorderIter_normalCase() {
+		TreeNode root = generateTree();
+		List<Integer> res = BinaryTree.preorderIter(root);
+		var actual = getPreorderResult();
+		assertIterableEquals(res, actual);
+	}
+
+	@Test
+	void inorderIter_normalCase() {
+		TreeNode root = generateTree();
+		List<Integer> res = BinaryTree.inorderIter(root);
+		var actual = getInorderResult();
+		assertIterableEquals(res, actual);
+	}
+
+	@Test
+	void postorderIter_normalCase() {
+		TreeNode root = generateTree();
+		List<Integer> res = BinaryTree.postorderIter(root);
+		var actual = getPostorderResult();
+		assertIterableEquals(res, actual);
+	}
+
+	private TreeNode generateTree() {
 		TreeNode root = new TreeNode(1);
 		root.setLeft(new TreeNode(2));
 		root.setRight(new TreeNode(3));
 		root.getLeft().setRight(new TreeNode(4));
 		return root;
+	}
+
+	private List<Integer> getInorderResult() {
+		var actual = new ArrayList<Integer>();
+		actual.add(2);
+		actual.add(4);
+		actual.add(1);
+		actual.add(3);
+		return actual;
+	}
+
+	private List<Integer> getPreorderResult() {
+		var actual = new ArrayList<Integer>();
+		actual.add(1);
+		actual.add(2);
+		actual.add(4);
+		actual.add(3);
+		return actual;
+	}
+
+	private List<Integer> getPostorderResult() {
+		var actual = new ArrayList<Integer>();
+		actual.add(4);
+		actual.add(2);
+		actual.add(3);
+		actual.add(1);
+		return actual;
 	}
 }
