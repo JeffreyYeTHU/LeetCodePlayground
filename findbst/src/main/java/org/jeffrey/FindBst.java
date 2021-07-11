3bfbea5f231a141cc1741bf6d39552f119b87b6d
Jeffrey <yejian_1988@126.com>
2021-07-11 10:11:07 +0800

    Add skeliton

--- findbst/src/main/java/org/jeffrey/FindBst.java
+++ findbst/src/main/java/org/jeffrey/FindBst.java
@@ -1,9 +1,24 @@
 package org.jeffrey;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FindBst {
-    public List<TreeNode> generateTrees(int n) {
+    public static List<TreeNode> generateTrees(int n) {
+        // create a array to track if a value already in the tree
+        boolean[] addedInBst = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            addedInBst[i] = false;
+        }
 
+        // build the bst one by one
+        List<TreeNode> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        return result;
     }
 }
--- findbst/src/test/java/org/jeffrey/FindBstTest.java
+++ findbst/src/test/java/org/jeffrey/FindBstTest.java
@@ -2,11 +2,18 @@ package org.jeffrey;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class FindBstTest {
 
     @Test
     public void generateTrees() {
+        List<TreeNode> expected = new LinkedList<>();
+        expected.add(new TreeNode(1));
+        List<TreeNode> actual = FindBst.generateTrees(1);
+        assertEquals(expected, actual);
     }
 }
