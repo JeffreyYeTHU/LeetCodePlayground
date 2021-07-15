cfe381ed75032ace6b6ec564cbe57f031dcddb70
Jian <yejian_1988@outlook.com>
2021-07-15 11:59:52 +0800

    reverse k group

--- reverselist/src/main/java/leetcode/ReverseList.java
+++ reverselist/src/main/java/leetcode/ReverseList.java
@@ -1,30 +1,30 @@
 package leetcode;
 
 public class ReverseList {
-    ListNode successor = null;  // for reverseN
+    ListNode successor = null; // for reverseN
 
-    public ListNode reverseBetween(ListNode head, int m, int n){
-        if (m == 1){
-            return  reverseN(head, n);
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
         }
         head.next = reverseBetween(head.next, m - 1, n - 1);
         return head;
     }
 
-    public ListNode reverseN(ListNode head, int n){
-        if (n == 1){
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
             successor = head.next;
-            return  head;
+            return head;
         }
         ListNode last = reverseN(head.next, n - 1);
         head.next.next = head;
         head.next = successor;
-        return  last;
+        return last;
     }
 
     public ListNode reverseBetweenIterative(ListNode head, int left, int right) {
-        if (left == right){
-            return  head;
+        if (left == right) {
+            return head;
         }
 
         ListNode curr = head;
@@ -33,15 +33,15 @@ public class ReverseList {
         ListNode leftNode = null;
         ListNode rightNode = null;
         int index = 1;
-        while (index <= right){
+        while (index <= right) {
             ListNode next = curr.next;
-            if (index < left){
+            if (index < left) {
                 // do nothing
-            } else if (index == left){
+            } else if (index == left) {
                 // left boundary
                 preLeftNode = pre;
                 leftNode = curr;
-            } else if (index > left && index < right){
+            } else if (index > left && index < right) {
                 // reverse
                 curr.next = pre;
             } else {
@@ -59,10 +59,33 @@ public class ReverseList {
             index++;
         }
 
-        if (left > 1){
+        if (left > 1) {
             return head;
         } else {
             return rightNode;
         }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // base case
+        int count = getListCount(head);
+        if (count <= k)
+            return head;
+
+        // recursive
+
+    }
+
+    private int getListCount(ListNode head) {
+        int count = 1;
+        var curr = head;
+        while (curr.next != null)
+            count++;
+        return count;
+    }
+
+    private List<ListNode> splitList(ListNode head, int k) {
+        firstHead = head;
+
     }
 }
