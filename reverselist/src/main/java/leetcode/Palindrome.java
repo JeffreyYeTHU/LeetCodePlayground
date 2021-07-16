e79a9d26a92aca6bfcfb82ed1e40724296edb514
Jian <yejian_1988@outlook.com>
2021-07-16 11:57:47 +0800

    Use slow/fast pointer to judge palindrome

--- reverselist/src/main/java/leetcode/Palindrome.java
+++ reverselist/src/main/java/leetcode/Palindrome.java
@@ -23,5 +23,43 @@ public final class Palindrome {
         return res;
     }
 
+    // second solution, use slow/fast pointer
+    public boolean isPalindromeUsePointer(ListNode head) {
+        // find middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // when total is odd, need advance slow one more
+        if (fast != null)
+            slow = slow.next;
+
+        // revere the second half
+        ListNode right = reverse(slow);
+
+        // do comparison
+        ListNode left = head;
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
     }
 }
