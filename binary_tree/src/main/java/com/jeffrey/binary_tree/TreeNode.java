5f125bc90b00e6fc16c7a96cd72023e0d9beba06
Jeffrey <yejian_1988@126.com>
2021-07-17 19:49:45 +0800

    Binary tree related

--- binary_tree/src/main/java/com/jeffrey/binary_tree/TreeNode.java
+++ binary_tree/src/main/java/com/jeffrey/binary_tree/TreeNode.java
@@ -1,39 +1,14 @@
 package com.jeffrey.binary_tree;
 
 public final class TreeNode {
-    private int val;
-    private TreeNode left;
-    private TreeNode right;
-
-    public TreeNode(int val) {
-        this.setVal(val);
-    }
-
-    public TreeNode getLeft() {
-        return left;
-    }
-
-    public void setLeft(TreeNode left) {
-        this.left = left;
-    }
-
-    public TreeNode getRight() {
-        return right;
-    }
-
-    public void setRight(TreeNode right) {
-        this.right = right;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
-    }
-
-    public boolean hasNoChildren() {
-        return this.getLeft() == null && this.getRight() == null;
+        this.left = left;
+        this.right = right;
     }
 }
