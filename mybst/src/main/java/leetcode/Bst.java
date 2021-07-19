896ae276fc397f6955a07b620547d74feebb6f8a
Jian <yejian_1988@outlook.com>
2021-07-19 14:28:47 +0800

    bst delete

--- mybst/src/main/java/leetcode/Bst.java
+++ mybst/src/main/java/leetcode/Bst.java
@@ -16,5 +16,41 @@ public class Bst {
         return root;
     }
 
-    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        if (root.val == key) { // found
+            // case a: both are null
+            if (root.left == null && root.right == null)
+                return null;
+
+            // case b: left or right is null
+            if (root.left == null)
+                return root.right;
+            if (root.right == null)
+                return root.left;
+
+            // case c: both not null
+            TreeNode max = getMax(root.left);
+            root.val = max.val;
+            root.left = deleteNode(root.left, max.val);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            root.left = deleteNode(root.left, key);
+        }
+
+        return root;
+    }
+
+    private TreeNode getMax(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode max = root;
+        while (max.right != null) {
+            max = max.right;
+        }
+        return max;
+    }
 }
