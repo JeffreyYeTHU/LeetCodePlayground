b09e30a6feb21c130d9c55a07cf40c342ea14b0e
Jian <yejian_1988@outlook.com>
2021-07-19 11:07:27 +0800

    convert bst to greater tree

---
+++ mybst/src/main/java/leetcode/GreeterTree.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class GreeterTree {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null)
+            return root;
+
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+
+        return root;
+    }
+}
