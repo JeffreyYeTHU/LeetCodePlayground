89cf1cb7e3c099835f3e0a6299648f93ac2eb009
Jian <yejian_1988@outlook.com>
2021-07-19 15:56:51 +0800

    valid bst

--- mybst/src/main/java/leetcode/Bst.java
+++ mybst/src/main/java/leetcode/Bst.java
@@ -53,4 +53,35 @@ public class Bst {
         }
         return max;
     }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+
+        return root;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null)
+            return true;
+
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
 }
