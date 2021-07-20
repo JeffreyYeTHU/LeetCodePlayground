1d2613206fb96eeb2b95fc66636ad06363cc7a48
Jeffrey <yejian_1988@126.com>
2021-07-21 00:08:51 +0800

    max sum bst

--- mybst/src/main/java/leetcode/Bst.java
+++ mybst/src/main/java/leetcode/Bst.java
@@ -143,4 +143,33 @@ public class Bst {
 
         return res;
     }
+
+    public int maxSumBST(TreeNode root) {
+        traverse(root);
+        return maxSum;
+    }
+
+    int maxSum = 0;
+
+    // return int[]{isBst, min, max, sum}
+    int[] traverse(TreeNode root) {
+        if (root == null)
+            return new int[] { 1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0 };
+
+        int[] left = traverse(root.left);
+        int[] right = traverse(root.right);
+
+        int[] res = new int[4];
+        if (left[0] == 1 && right[0] == 1 && root.val > left[2] && root.val < right[1]) {
+            res[0] = 1;
+            res[1] = Math.min(root.val, left[1]);
+            res[2] = Math.max(root.val, right[2]);
+            res[3] = left[3] + right[3] + root.val;
+            maxSum = Math.max(maxSum, res[3]);
+        } else {
+            res[0] = 0;
+        }
+
+        return res;
+    }
 }
