dee538e2e3cbb9c1664281f29caa7ca3bd2ef98a
Jian <yejian_1988@outlook.com>
2021-08-02 16:33:37 +0800

    #344

--- algorithm-design/TwoPointer.cs
+++ algorithm-design/TwoPointer.cs
@@ -85,5 +85,20 @@ namespace LeetCode
             }
             return new int[] { -1, -1 };
         }
+
+        // 344. 反转字符串
+        public void ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+            while (left < right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+        }
     }
 }
