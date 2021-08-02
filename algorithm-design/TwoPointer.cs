6db7f5ab105b5cbd1195ea7d3154f606616b6a35
Jian <yejian_1988@outlook.com>
2021-08-02 16:48:16 +0800

    #19

--- algorithm-design/TwoPointer.cs
+++ algorithm-design/TwoPointer.cs
@@ -100,5 +100,27 @@ namespace LeetCode
                 right--;
             }
         }
+
+        // 19. 删除链表的倒数第 N 个结点
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null) return null;
+            var slow = head;
+            var fast = head;
+            while (n > 0)
+            {
+                fast = fast.next;
+                n--;
+            }
+            if (fast == null)
+                return head.next;
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return head;
+        }
     }
 }
