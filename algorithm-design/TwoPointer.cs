b04f4b592397a9f28b40f202beaf5aa88feea498
Jian <yejian_1988@outlook.com>
2021-08-02 16:09:49 +0800

    #142

--- algorithm-design/TwoPointer.cs
+++ algorithm-design/TwoPointer.cs
@@ -15,6 +15,27 @@ namespace LeetCode
             }
             return false;
         }
+
+        // 142. 环形链表 II
+        public ListNode DetectCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (slow == fast) break;
+            }
+            if (fast == null || fast.next == null)
+                return null;
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
         }
     }
 }
