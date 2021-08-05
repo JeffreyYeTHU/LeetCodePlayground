09e9d2fd0e179f9aa9c1f3542be1908feb2db1ef
Jian <yejian_1988@outlook.com>
2021-08-05 16:44:08 +0800

    #322

--- algorithm-design/DinamicPrograming.cs
+++ algorithm-design/DinamicPrograming.cs
@@ -19,5 +19,27 @@ namespace LeetCode
             return res;
         }
 
+        // 322
+        public int CoinChange(int[] coins, int amount)
+        {
+            // base case
+            if (amount == 0) return 0;
+            int[] dp = new int[amount + 1];
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++)
+                dp[i] = amount + 1;
+
+            // do state transfer
+            for (int i = 1; i <= amount; i++)
+            {
+                foreach (int coin in coins)
+                {
+                    if (i - coin < 0) continue;
+                    dp[i] = System.Math.Min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+
+            return dp[amount] == amount + 1 ? -1 : dp[amount];
+        }
     }
 }
