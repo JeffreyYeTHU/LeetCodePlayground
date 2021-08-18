ecc9ea82a9072debe3d4d0f8a7ea360321b79db1
Jian <yejian_1988@outlook.com>
2021-08-18 10:57:01 +0800

    #104 csharp

---
+++ 104.二叉树的最大深度.cs
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=csharp
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        int leftMax = MaxDepth(root.left);
+        int rightMax = MaxDepth(root.right);
+        return Math.Max(leftMax, rightMax) + 1;
+    }
+}
+// @lc code=end
+
