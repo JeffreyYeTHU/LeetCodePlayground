018844a1370da9284b04dbeb8d2137a74b68b11c
Jian <yejian_1988@outlook.com>
2021-08-19 15:04:16 +0800

    #64 csharp

--- 64.最小路径和.cs
+++ 64.最小路径和.cs
@@ -12,36 +12,24 @@ public class Solution
         // dp[i, j] = min path sum from grid[i, j] to grid[m-1, n-1]
         int m = grid.Length;
         int n = grid[0].Length;
-        int[,] dp = new int[m, n];
-        for (int i = m - 1; i >= 0; i--)
+        int[] dp = new int[n];
+
+        // last row
+        dp[n - 1] = grid[m - 1][n - 1];
+        for (int j = n - 2; j >= 0; j--)
+            dp[j] = dp[j + 1] + grid[m - 1][j];
+
+        // update
+        for (int i = m - 2; i >= 0; i--)
         {
             for (int j = n - 1; j >= 0; j--)
             {
-                if (i == m - 1 && j == n - 1)
-                    dp[i, j] = grid[i][j];
-                else
-                {
-                    int right = (j == n - 1) ? int.MaxValue : dp[i, j + 1];
-                    int down = (i == m - 1) ? int.MaxValue : dp[i + 1, j];
-                    dp[i, j] = Math.Min(right, down) + grid[i][j];
-                }
+                int right = (j == n - 1) ? int.MaxValue : dp[j + 1];
+                int down = dp[j];
+                dp[j] = Math.Min(right, down) + grid[i][j];
             }
         }
-        //Print(dp);
-        return dp[0, 0];
-    }
-
-    void Print(int[,] dp)
-    {
-        Console.WriteLine("dp=");
-        for (int i = 0; i < dp.GetLength(0); i++)
-        {
-            for (int j = 0; j < dp.GetLength(1); j++)
-            {
-                Console.Write(dp[i, j] + ", ");
-            }
-            Console.WriteLine();
-        }
+        return dp[0];
     }
 }
 // @lc code=end
