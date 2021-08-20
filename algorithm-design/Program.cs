01f0f88cdb7a4269952e88b6a0c0d6d3fe1e9896
Jian <yejian_1988@outlook.com>
2021-08-20 12:24:27 +0800

    #514 csharp

--- algorithm-design/Program.cs
+++ algorithm-design/Program.cs
@@ -8,16 +8,39 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 10, 9, 2, 5, 3, 7, 101, 18 };
-            var sortedData = new SortedList<int, int>();  // make value=key
-            int[] dp = new int[nums.Length];
-            for (int i = 0; i < nums.Length; i++)
+            string ring = "godding";
+            string key = "gd";
+
+            // build dic
+            int m = ring.Length;
+            int n = key.Length;
+            var varToIdx = new Dictionary<char, List<int>>();
+            for (int i = 0; i < m; i++)
             {
-                if (!sortedData.ContainsKey(nums[i]))
-                    sortedData.Add(nums[i], nums[i]);
-                dp[i] = sortedData.IndexOfKey(nums[i]) + 1;
+                if (!varToIdx.ContainsKey(ring[i]))
+                    varToIdx.Add(ring[i], new List<int>());
+                varToIdx[ring[i]].Add(i);
             }
-            Console.WriteLine(dp);
+
+            // dp[i, j] = start from ring[i], the min steps to key[j..n-1]
+            int[,] dp = new int[m, n + 1];
+            for (int i = 0; i < m; i++)
+                dp[i, n] = 0;
+            for (int j = n - 1; j >= 0; j--)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    List<int> kIdx = varToIdx[key[j]];
+                    var disToK = kIdx
+                        .Select(idx =>
+                        {
+                            int d = i - idx > 0 ? i - idx : idx - i;  // d >=0
+                            return Math.Min(d, m - d) + 1 + dp[idx, j + 1];
+                        })
+                        .ToList();
+                    dp[i, j] = disToK.Min();
+                }
+            }
         }
     }
 }
