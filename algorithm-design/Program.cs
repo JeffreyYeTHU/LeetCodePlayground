461dfadf4c415383930db2952514ca29d17313a5
Jeffrey <yejian_1988@126.com>
2021-08-21 17:51:38 +0800

    #94. 144 csharp

--- algorithm-design/Program.cs
+++ algorithm-design/Program.cs
@@ -8,37 +8,20 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            string ring = "godding";
-            string key = "gd";
-
-            // build dic
-            int m = ring.Length;
-            int n = key.Length;
-            var varToIdx = new Dictionary<char, List<int>>();
-            for (int i = 0; i < m; i++)
-            {
-                if (!varToIdx.ContainsKey(ring[i]))
-                    varToIdx.Add(ring[i], new List<int>());
-                varToIdx[ring[i]].Add(i);
-            }
-
-            // dp[i, j] = start from ring[i], the min steps to key[j..n-1]
-            int[,] dp = new int[m, n + 1];
-            for (int i = 0; i < m; i++)
-                dp[i, n] = 0;
-            for (int j = n - 1; j >= 0; j--)
-            {
-                for (int i = 0; i < m; i++)
-                {
-                    List<int> kIdx = varToIdx[key[j]];
-                    var disToK = kIdx
-                        .Select(idx =>
-                        {
-                            int d = i - idx > 0 ? i - idx : idx - i;  // d >=0
-                            return Math.Min(d, m - d) + 1 + dp[idx, j + 1];
-                        })
-                        .ToList();
-                    dp[i, j] = disToK.Min();
+            var s = new Stack<TreeNode>();
+            var res = new List<int>();
+            var root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+            TreeNode p = root;
+            while (p != null || s.Count != 0){
+                if(p != null){
+                    s.Push(p);
+                    p = p.left;
+                } else {
+                    var node = s.Pop();
+                    res.Add(node.val);
+                    p = node.right;
                 }
             }
         }
