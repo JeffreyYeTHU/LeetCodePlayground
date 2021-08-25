77841fbf6c89d5c0763a7914370dea5619eea3e4
Jian <yejian_1988@outlook.com>
2021-08-25 10:13:50 +0800

    #94 a general method

--- 94.二叉树的中序遍历.cs
+++ 94.二叉树的中序遍历.cs
@@ -20,27 +20,60 @@
  */
 public class Solution
 {
+    Stack<TreeNode> stk = new Stack<TreeNode>();
+    List<int> inorder = new List<int>();
+    TreeNode lastVisit = new TreeNode(-1);
     public IList<int> InorderTraversal(TreeNode root)
     {
         // solution 1:
         // Traverse(root);
         // return res;
 
-        // solution 2:
-        var s = new Stack<TreeNode>();
-        var res = new List<int>();
-        TreeNode p = root;
-        while (p != null || s.Count != 0){
-            if(p != null){
-                s.Push(p);
-                p = p.left;
-            } else {
-                var node = s.Pop();
-                res.Add(node.val);
-                p = node.right;
+        // // solution 2:
+        // var s = new Stack<TreeNode>();
+        // var res = new List<int>();
+        // TreeNode p = root;
+        // while (p != null || s.Count != 0){
+        //     if(p != null){
+        //         s.Push(p);
+        //         p = p.left;
+        //     } else {
+        //         var node = s.Pop();
+        //         res.Add(node.val);
+        //         p = node.right;
+        //     }
+        // }
+        // return res;
+
+        // solution 3
+        if (root == null)
+            return inorder;
+
+        PushLeftBranch(root);
+        while (stk.Count != 0)
+        {
+            var p = stk.Peek();
+            if ((p.left == null || p.left == lastVisit) && p.right != lastVisit)
+            {
+                inorder.Add(p.val);
+                PushLeftBranch(p.right);
+            }
+            if (p.right == null || p.right == lastVisit)
+            {
+                lastVisit = p;
+                stk.Pop();
             }
         }
-        return res;
+        return inorder;
+    }
+
+    void PushLeftBranch(TreeNode p)
+    {
+        while (p != null)
+        {
+            stk.Push(p);
+            p = p.left;
+        }
     }
 
     // List<int> res = new List<int>();
