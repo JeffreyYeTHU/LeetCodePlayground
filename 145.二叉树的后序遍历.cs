a4580bc38ee75e10222badf4135e51bd3d0963b6
Jian <yejian_1988@outlook.com>
2021-08-25 10:19:51 +0800

    #145 a general method

--- 145.二叉树的后序遍历.cs
+++ 145.二叉树的后序遍历.cs
@@ -18,34 +18,67 @@
  *     }
  * }
  */
-public class Solution {
-    public IList<int> PostorderTraversal(TreeNode root) {
+public class Solution
+{
+    Stack<TreeNode> stk = new Stack<TreeNode>();
+    List<int> postorder = new List<int>();
+    TreeNode lastVisit = new TreeNode(-1);
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
         // solution 1:
         // Traverse(root);
         // return postorder;
 
         // solution 2:
-        var res = new List<int>();
-        var s = new Stack<TreeNode>();
-        TreeNode curr = root;
-        TreeNode lastVisit = root;
-        while(curr != null || s.Count !=0){
-            while(curr != null){
-                s.Push(curr);
-                curr = curr.left;
-            }
+        // var res = new List<int>();
+        // var s = new Stack<TreeNode>();
+        // TreeNode curr = root;
+        // TreeNode lastVisit = root;
+        // while(curr != null || s.Count !=0){
+        //     while(curr != null){
+        //         s.Push(curr);
+        //         curr = curr.left;
+        //     }
 
-            curr = s.Peek();
-            if(curr.right == null || lastVisit == curr.right){
-                res.Add(curr.val);
-                s.Pop();
-                lastVisit = curr;
-                curr = null;
-            } else {
-                curr = curr.right;
+        //     curr = s.Peek();
+        //     if(curr.right == null || lastVisit == curr.right){
+        //         res.Add(curr.val);
+        //         s.Pop();
+        //         lastVisit = curr;
+        //         curr = null;
+        //     } else {
+        //         curr = curr.right;
+        //     }
+        // }
+        // return res;
+
+        // solution 3:
+        if (root == null)
+            return postorder;
+
+        PushLeftBranch(root);
+        while (stk.Count != 0)
+        {
+            var p = stk.Peek();
+            if ((p.left == null || p.left == lastVisit) && p.right != lastVisit)
+                PushLeftBranch(p.right);
+            if (p.right == null || p.right == lastVisit)
+            {
+                postorder.Add(p.val);
+                lastVisit = p;
+                stk.Pop();
             }
         }
-        return res;
+        return postorder;
+    }
+
+    void PushLeftBranch(TreeNode p)
+    {
+        while (p != null)
+        {
+            stk.Push(p);
+            p = p.left;
+        }
     }
 
     // List<int> postorder = new List<int>();
