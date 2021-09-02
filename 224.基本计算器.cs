c7637e7cd28c469cd1f0411bc4a46e51842834e9
Jian <yejian_1988@outlook.com>
2021-09-02 15:49:24 +0800

    #224 csharp

--- 224.基本计算器.cs
+++ 224.基本计算器.cs
@@ -9,118 +9,48 @@ public class Solution
 {
     public int Calculate(string s)
     {
-        var sb = new StringBuilder();
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (s[i] != ' ')
-                sb.Append(s[i]);
-        }
-        string sNospace = sb.ToString();
+        return Compute(new LinkedList<char>(s.ToCharArray()));
+    }
 
-        parenPos = new HashSet<int>();
-        leftParenToRight = new Dictionary<int, int>();
+    int Compute(LinkedList<char> strChars)
+    {
         var stk = new Stack<int>();
-        for (int i = 0; i < sNospace.Length; i++)
+        char op = '+';
+        int num = 0;
+        while (strChars.Count != 0)
         {
-            if (sNospace[i] == '(')
+            char c = strChars.First();
+            strChars.RemoveFirst();
+
+            if (c == '(')
+                num = Compute(strChars);
+
+            if (char.IsDigit(c))
+                num = num * 10 + (c - '0');
+
+            if ((!char.IsDigit(c) && c != ' ') || strChars.Count == 0)
             {
-                parenPos.Add(i);
-                stk.Push(i);
-            }
-            else if (sNospace[i] == ')')
-            {
-                parenPos.Add(i);
-                leftParenToRight.Add(stk.Pop(), i);
-            }
-        }
-        return Compute(sNospace, 0, sNospace.Length - 1);
-    }
-
-    HashSet<int> parenPos;
-    Dictionary<int, int> leftParenToRight;
-
-    // compute s[i..j]
-    int Compute(string s, int i, int j)
-    {
-        // base case
-        if (i > j)
-            return 0;
-
-        bool hasParen = RangeContainsParen(s, i, j, out int leftParenPos);
-        if (hasParen)
-        {
-            // open the first level of parenthese
-            int res = Compute(s, i, leftParenPos - 2);
-            int rightParenPos = 0;
-            while (parenPos.Contains(leftParenPos))
-            {
-                rightParenPos = leftParenToRight[leftParenPos];
-                if (leftParenPos == i || s[leftParenPos - 1] == '+')
-                    res += Compute(s, leftParenPos + 1, rightParenPos - 1);
-                else
-                    res -= Compute(s, leftParenPos + 1, rightParenPos - 1);
-
-                bool stillHasLevelOne = RangeContainsParen(s, rightParenPos + 2, j, out leftParenPos);
-                if (stillHasLevelOne)
+                switch (op)
                 {
-                    if (s[rightParenPos + 1] == '+')
-                        res += Compute(s, rightParenPos + 2, leftParenPos - 2);
-                    else
-                        res -= Compute(s, rightParenPos + 2, leftParenPos - 2);
-                }
-            }
-            if (rightParenPos == j || s[rightParenPos + 1] == '+')
-                res += Compute(s, rightParenPos + 2, j);
-            else
-                res -= Compute(s, rightParenPos + 2, j);
-            return res;
-        }
-        else
-        {
-            int res = 0;
-            int slow = i - 1;
-            int fast = i;
-            if (s[i] == '+' || s[i] == '-')
-            {
-                slow++;
-                fast++;
-            }
-            while (fast <= j)
-            {
-                while (fast <= j)
-                {
-                    if (s[fast] == '+' || s[fast] == '-')
+                    case '+':
+                        stk.Push(num);
                         break;
-                    fast++;
+
+                    case '-':
+                        stk.Push(-num);
+                        break;
                 }
 
-                int curr = int.Parse(s.Substring(slow + 1, fast - slow - 1));
-                if (slow == i - 1 || s[slow] == '+')
-                    res += curr;
-                else
-                    res -= curr;
-
-                slow = fast;
-                fast++;
+                // update sign and num
+                num = 0;
+                op = c;
             }
-            return res;
-        }
-    }
 
-    // for s[i..j], check if it has parenthese, if so, set the first
-    //  left paren pos to parameter 'leftParen'
-    bool RangeContainsParen(string s, int i, int j, out int leftParenPos)
-    {
-        for (int k = i; k <= j; k++)
-        {
-            if (parenPos.Contains(k))
-            {
-                leftParenPos = k;
-                return true;
-            }
+            if (c == ')')
+                break;
         }
-        leftParenPos = -1;
-        return false;
+
+        return stk.Sum();
     }
 }
 // @lc code=end
--- algorithm-design/Program.cs
+++ algorithm-design/Program.cs
@@ -7,120 +7,51 @@ namespace LeetCode
 {
     class Program
     {
-        static HashSet<int> parenPos;
-        static Dictionary<int, int> leftParenToRight;
         static void Main(string[] args)
         {
-            string s = "1-(3+5-2+(3+19-(3-1-4+(9-4-(4-(1+(3)-2)-5)+8-(3-5)-1)-4)-5)-4+3-9)-4-(3+2-5)-10";
-            var sb = new StringBuilder();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] != ' ')
-                    sb.Append(s[i]);
-            }
-            string sNospace = sb.ToString();
+            string s = "1 + 1 ";
+            int res = Compute(new LinkedList<char>(s.ToCharArray()));
+        }
 
-            parenPos = new HashSet<int>();
-            leftParenToRight = new Dictionary<int, int>();
+        static int Compute(LinkedList<char> strChars)
+        {
             var stk = new Stack<int>();
-            for (int i = 0; i < sNospace.Length; i++)
+            char op = '+';
+            int num = 0;
+            while (strChars.Count != 0)
             {
-                if (sNospace[i] == '(')
-                {
-                    parenPos.Add(i);
-                    stk.Push(i);
-                }
-                else if (sNospace[i] == ')')
-                {
-                    parenPos.Add(i);
-                    leftParenToRight.Add(stk.Pop(), i);
-                }
-            }
-            int res = Compute(sNospace, 0, sNospace.Length - 1);
-        }
+                char c = strChars.First();
+                strChars.RemoveFirst();
 
-        static int Compute(string s, int i, int j)
-        {
-            // base case
-            if (i > j)
-                return 0;
+                if (c == '(')
+                    num = Compute(strChars);
 
-            bool hasParen = RangeContainsParen(s, i, j, out int leftParenPos);
-            if (hasParen)
-            {
-                // open the first level of parenthese
-                int res = Compute(s, i, leftParenPos - 2);
-                int rightParenPos = 0;
-                while (parenPos.Contains(leftParenPos))
+                if (char.IsDigit(c))
+                    num = num * 10 + (c - '0');
+
+                if ((!char.IsDigit(c) && c != ' ') || strChars.Count == 0)
                 {
-                    rightParenPos = leftParenToRight[leftParenPos];
-                    if (leftParenPos == i || s[leftParenPos - 1] == '+')
-                        res += Compute(s, leftParenPos + 1, rightParenPos - 1);
-                    else
-                        res -= Compute(s, leftParenPos + 1, rightParenPos - 1);
-
-                    bool stillHasLevelOne = RangeContainsParen(s, rightParenPos + 2, j, out leftParenPos);
-                    if (stillHasLevelOne)
+                    switch (op)
                     {
-                        if (s[rightParenPos + 1] == '+')
-                            res += Compute(s, rightParenPos + 2, leftParenPos - 2);
-                        else
-                            res -= Compute(s, rightParenPos + 2, leftParenPos - 2);
-                    }
-                }
-                if (rightParenPos == j || s[rightParenPos + 1] == '+')
-                    res += Compute(s, rightParenPos + 2, j);
-                else
-                    res -= Compute(s, rightParenPos + 2, j);
-                return res;
-            }
-            else
-            {
-                int res = 0;
-                int slow = i - 1;
-                int fast = i;
-                if (s[i] == '+' || s[i] == '-')
-                {
-                    slow++;
-                    fast++;
-                }
-                while (fast <= j)
-                {
-                    while (fast <= j)
-                    {
-                        if (s[fast] == '+' || s[fast] == '-')
+                        case '+':
+                            stk.Push(num);
                             break;
-                        fast++;
+
+                        case '-':
+                            stk.Push(-num);
+                            break;
                     }
 
-                    int curr = int.Parse(s.Substring(slow + 1, fast - slow - 1));
-                    if (slow == i - 1 || s[slow] == '+')
-                        res += curr;
-                    else
-                        res -= curr;
-
-                    slow = fast;
-                    fast++;
+                    // update sign and num
+                    num = 0;
+                    op = c;
                 }
-                return res;
-            }
-        }
 
-        // for s[i..j], check if it has parenthese, if so, set the first
-        //  left paren pos to parameter 'leftParen'
-        static bool RangeContainsParen(string s, int i, int j, out int leftParenPos)
-        {
-            for (int k = i; k <= j; k++)
-            {
-                if (parenPos.Contains(k))
-                {
-                    leftParenPos = k;
-                    return true;
-                }
+                if (c == ')')
+                    break;
             }
-            leftParenPos = -1;
-            return false;
-        }
 
+            return stk.Sum();
+        }
     }
 }
