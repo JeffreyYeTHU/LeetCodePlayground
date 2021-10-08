3d512d83a7481d0c9cc28de45387759d641dc481
Jeffrey <yejian_1988@126.com>
2021-10-08 23:08:55 +0800

    #1039 csharp

--- csharp/1039.多边形三角剖分的最低得分.cs
+++ csharp/1039.多边形三角剖分的最低得分.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Collections.Generic;
+using System;
 /*
  * @lc app=leetcode.cn id=1039 lang=csharp
  *
@@ -12,7 +15,7 @@ public class Solution {
         // return minScore;
 
         // A different definition of recursive function
-        
+        return Dp(values, 0, values.Length -1);
     }
 
     // int minScore = int.MaxValue;
@@ -36,6 +39,32 @@ public class Solution {
     //         options.Insert(i, curr);
     //     }
     // }
+
+    // Dp(i, j) : min score of form by values[i] to values[j]
+    Dictionary<(int ,int), int> memo = new Dictionary<(int, int), int>();
+    int Dp(int[] values, int i, int j){
+        // base case
+        if(j - i == 2){
+            return values[i] * values[i+1] * values[i+2];
+        }
+
+        if(memo.ContainsKey((i,j)))
+            return memo[(i,j)];
+        int minScore = int.MaxValue;
+        for(int k = i+1; k < j; k++){
+            int secletTriangle = values[i] * values[k] * values[j];
+            int currScore;
+            if(k == i+1)
+                currScore = secletTriangle + Dp(values, k, j);
+            else if (k == j -1)
+                currScore = secletTriangle + Dp(values, i, k);
+            else
+                currScore = secletTriangle + Dp(values, i, k) + Dp(values, k, j);
+            minScore = Math.Min(minScore, currScore);
+        }
+        memo.Add((i,j), minScore);
+        return minScore;
+    }
 }
 // @lc code=end
 
