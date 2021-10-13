263f5ad763d5608f7098a012049d1b07320de7e7
Jian <yejian_1988@outlook.com>
2021-10-13 17:17:32 +0800

    #54 csharp TBC

---
+++ csharp/54.螺旋矩阵.cs
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=csharp
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+public class Solution
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        var res = new List<int>();
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int large = Math.Max(m, n);
+        int row = 0;
+        int col = 0;
+        for (int cnt = 0; cnt < (large + 1) / 2; cnt++)
+        {
+            // right
+            for (; col < n; col++)
+            {
+                if (matrix[row][col] == int.MaxValue)
+                    break;
+                res.Add(matrix[row][col]);
+                matrix[row][col] = int.MaxValue;
+            }
+            col--;
+            // down
+            for (; row < m; row++)
+            {
+                if (matrix[row][col] == int.MaxValue)
+                    break;
+                res.Add(matrix[row][col]);
+                matrix[row][col] = int.MaxValue;
+            }
+            row--;
+            // left
+            for (; col >= 0; col--)
+            {
+                if (matrix[row][col] == int.MaxValue)
+                    break;
+                res.Add(matrix[row][col]);
+                matrix[row][col] = int.MaxValue;
+            }
+            col++;
+            // up
+            for (; row >= 0; row--)
+            {
+                if (matrix[row][col] == int.MaxValue)
+                    break;
+                res.Add(matrix[row][col]);
+                matrix[row][col] = int.MaxValue;
+            }
+            row++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
