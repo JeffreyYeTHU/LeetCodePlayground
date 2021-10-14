f86be676d24cd63a1151e09231769bd9fb1342fd
Jian <yejian_1988@outlook.com>
2021-10-14 11:15:03 +0800

    #71 csharp use 2 pointer

---
+++ csharp/71.简化路径.cs
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=csharp
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+public class Solution
+{
+    public string SimplifyPath(string path)
+    {
+        int len = path.Length;
+        if (len <= 1)
+            return "/";
+        var res = new List<string>();
+        res.Add("/");
+
+        // Solution 1 use 2 pointer
+        int slow = 0;
+        int fast = 1;
+        while (slow < len && fast < len)
+        {
+            // skip connective /
+            while (fast < len && path[fast] == '/')
+            {
+                fast++;
+                slow++;
+            }
+
+            // fast pointer forward to next /
+            while (fast < len && path[fast] != '/')
+                fast++;
+
+            // check current dir
+            string currDir = path.Substring(slow + 1, fast - slow - 1);
+            if (currDir == "" || currDir == ".")
+            {
+                // do nothing
+            }
+            else if (currDir == "..")
+            {
+                res = ToParent(res);
+            }
+            else
+            {
+                if (res.Count != 1)
+                    res.Add("/");
+                res.Add(currDir);
+            }
+
+            slow = fast;
+            fast++;
+        }
+
+        // Solution 2: split string
+
+
+        string simple = "";
+        foreach (var dir in res)
+        {
+            simple += dir;
+        }
+        return simple;
+    }
+
+    List<string> ToParent(List<string> path)
+    {
+        if (path.Count == 1)
+            return path;
+        else if (path.Count == 2)
+        {
+            path.RemoveAt(1);
+            return path;
+        }
+        else
+        {
+            path.RemoveAt(path.Count - 1);
+            path.RemoveAt(path.Count - 1);
+            return path;
+        }
+    }
+}
+// @lc code=end
+
