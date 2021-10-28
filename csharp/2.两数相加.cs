d56017f42cbb881fe00fe17a4641d559c93f5a26
Jian <yejian_1988@outlook.com>
2021-10-28 17:35:21 +0800

    #2 csharp

---
+++ csharp/2.两数相加.cs
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=csharp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        var p = dummy;
+        while (l1 != null || l2 != null)
+        {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int sum = a + b + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+            p.next = new ListNode(digit);
+            p = p.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry != 0)
+            p.next = new ListNode(carry);
+        return dummy.next;
+    }
+}
+// @lc code=end
+
