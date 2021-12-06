f7dc6dc64bb6edffda2c21fe7e7f8fb3c82648ae
Jeffrey <780336375@qq.com>
2021-12-06 16:13:30 +0800

    Add problem 651

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -12,6 +12,25 @@ namespace LeetCode
             //SortAlgoTest();
         }
 
+        // Problem 651 is not available in LeetCode, so I write my own
+        private static int Problem651(int n)
+        {
+            // dp[i] : for i operations, the max A's can get
+            if (n <= 6)
+                return n;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + 1;  // just insert A
+                for (int j = i - 3; j >= 2; j--)
+                {
+                    dp[i] = Math.Max(dp[i], dp[j] * (i - j - 3 + 1));
+                }
+            }
+            return dp[n];
+        }
+
         private static void SortAlgoTest()
         {
             int[] arr = new int[] { 2, 5, 3, 1, 4 };
