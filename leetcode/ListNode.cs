4edd0973a22436f0a76bfc131cc1382653b457b5
Your Name <you@example.com>
2022-01-01 21:48:32 +0800

    Test graph path

---
+++ leetcode/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode
+{
+    public class ListNode
+    {
+        public int Val { get; set; }
+        public ListNode Next { get; set; }
+        public ListNode(int val, ListNode next = null)
+        {
+            Val = val;
+            Next = next;
+        }
+    }
+}
