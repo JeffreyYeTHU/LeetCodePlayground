57a71d50d5f89d6008578f0ac46098d47fb85504
Your Name <you@example.com>
2022-01-06 20:47:24 +0800

    Impelment a FreqStack

---
+++ leetcode/FreqStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCode
+{
+    public class FreqStack
+    {
+        Dictionary<int, int> valToFreq;
+        Dictionary<int, Stack<int>> freqToVals;  // if a val have freq > 1, it will show up in mutiple stacks
+        int maxFreq;
+        public FreqStack()
+        {
+            maxFreq = 0;
+            valToFreq = new();
+            freqToVals = new();
+        }
+
+        public void Push(int val)
+        {
+            int newfreq;
+            if (valToFreq.ContainsKey(val))
+            {
+                valToFreq[val]++;
+                newfreq = valToFreq[val];
+            }
+            else
+            {
+                valToFreq.Add(val, 1);
+                newfreq = 1;
+            }
+            if (!freqToVals.ContainsKey(newfreq))
+                freqToVals.Add(newfreq, new Stack<int>());
+            freqToVals[newfreq].Push(val);
+            maxFreq = Math.Max(maxFreq, newfreq);
+        }
+
+        public int Pop()
+        {
+            var vals = freqToVals[maxFreq];
+            int res = vals.Pop();
+            valToFreq[res]--;
+            if(vals.Count == 0)
+            {
+                maxFreq--;
+            }
+            return res;
+        }
+    }
+}
